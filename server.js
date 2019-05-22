7122634af044187907ab2ddf1bd5be66a4f80680
Cruz Ortiz <cruzortiz099@gmail.com>
2019-05-22 13:04:56 -0400

    page done

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname)))
+
+app.get('/', (request, response) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    response.end(data)
+  })
+})
+
+app.listen(port)
